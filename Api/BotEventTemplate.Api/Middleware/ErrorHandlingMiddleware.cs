c7a7ff808f34913c2fe1834c31d7b7be99c41281
tuliorezende <tulio.rezende@outlook.com>
2018-08-06 17:58:00 -0300

    create error handling

---
+++ Api/BotEventTemplate.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BotEventManagement.Api.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var result = JsonConvert.SerializeObject(exception);
+
+            if (exception is WebException webException)
+                context.Response.StatusCode = (int)((HttpWebResponse)webException.Response).StatusCode;
+            else
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
