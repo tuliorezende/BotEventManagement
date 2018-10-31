7a93b3f547e2f0c0757d121235bdec64b9c4d900
tuliorezende <tulio.rezende@outlook.com>
2018-10-31 15:20:15 -0300

    change some configurations

--- Api/BotEventTemplate.Api/Startup.cs
+++ Api/BotEventTemplate.Api/Startup.cs
@@ -16,9 +16,14 @@ namespace BotEventTemplate.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment environment)
         {
-            Configuration = configuration;
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables().Build();
+
         }
 
         public IConfiguration Configuration { get; }
@@ -41,12 +46,14 @@ namespace BotEventTemplate.Api
                 c.IncludeXmlComments(xmlPath);
             });
 
+
             services.AddScoped<IEventService, EventService>();
             services.AddScoped<IEventParticipantService, EventParticipantsService>();
             services.AddScoped<IActivityService, ActivityService>();
             services.AddScoped<IUserTalksService, UserTalksService>();
             services.AddScoped<ISpeakerService, SpeakerService>();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
