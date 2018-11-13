fd44ff3abc2b5874b5ce757bb24e2e0c62bf699b
tuliorezende <tulio.rezende@outlook.com>
2018-11-13 09:22:47 -0200

    put some logs

--- Api/BotEventTemplate.Api/Startup.cs
+++ Api/BotEventTemplate.Api/Startup.cs
@@ -30,8 +30,13 @@ namespace BotEventTemplate.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Console.WriteLine("Configure Services - Begin");
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            Console.WriteLine("Configure Services - Before Database Configuration");
             services.AddDbContext<BotEventManagementContext>(options => options.UseSqlServer(Configuration["DefaultConnection"]));
+
+            Console.WriteLine("Configure Services - Before Swagger Configuration");
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info
@@ -42,9 +47,13 @@ namespace BotEventTemplate.Api
                 });
                 var basePath = AppContext.BaseDirectory;
                 var xmlPath = Path.Combine(basePath, "BotEventManagement.Api.xml");
+
+                Console.WriteLine($"Configure Services - Directory: {xmlPath}");
+
                 c.IncludeXmlComments(xmlPath);
             });
 
+            Console.WriteLine("Configure Services - Before Dependency Injection Configuration");
 
             services.AddScoped<IEventService, EventService>();
             services.AddScoped<IEventParticipantService, EventParticipantsService>();
@@ -57,15 +66,22 @@ namespace BotEventTemplate.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            Console.WriteLine("Configure Services - Before Environment Configuration");
+
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
             else
                 app.UseHsts();
 
             app.UseHttpsRedirection();
+
+            Console.WriteLine("Configure Services - Before Middleware Configuration");
             app.UseMiddleware<ErrorHandlingMiddleware>();
 
+            Console.WriteLine("Configure Services - Before Update Database Configuration");
             UpdateDatabase(app);
+
+            Console.WriteLine("Configure Services - Before Swagger Json Configuration");
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
