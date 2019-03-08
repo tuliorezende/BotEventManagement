2a8fa6f1ebbef1a9650c1f905998eacf2561cdc5
tuliorezende <tulio.rezende@outlook.com>
2019-03-08 16:00:47 -0300

    create front project and first screen

---
+++ Api/BotEventManagement.Web/Api/IEventManagerApi.cs
@@ -0,0 +1,15 @@
+﻿using BotEventManagement.Models.API;
+using RestEase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotEventManagement.Web.Api
+{
+    public interface IEventManagerApi
+    {
+        [Get("/api/Event")]
+        Task<List<EventRequest>> GetAllEvents();
+    }
+}
---
+++ Api/BotEventManagement.Web/Controllers/EventController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BotEventManagement.Web.Api;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotEventManagement.Web.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IEventManagerApi _eventManagerApi;
+
+        public EventController(IEventManagerApi eventManagerApi)
+        {
+            _eventManagerApi = eventManagerApi;
+        }
+        // GET: Event
+        public async Task<ActionResult> Index()
+        {
+            var events = await _eventManagerApi.GetAllEvents();
+            return View(events);
+        }
+
+        // GET: Event/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Event/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Event/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Event/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Event/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Event/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Event/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
