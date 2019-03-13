6a9d3232286f08e3a3d75050ec1d8492d2fa4ab8
tuliorezende <tulio.rezende@outlook.com>
2019-03-13 13:31:27 -0300

    put kestrel configurations

--- Api/BotEventManagement.Web/Program.cs
+++ Api/BotEventManagement.Web/Program.cs
@@ -19,6 +19,7 @@ namespace BotEventManagement.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
