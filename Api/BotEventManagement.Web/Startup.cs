d093c36e67fdabd2535a8ec42842820110261005
tuliorezende <tulio.rezende@outlook.com>
2019-03-13 08:31:29 -0300

    change startup file

--- Api/BotEventManagement.Web/Startup.cs
+++ Api/BotEventManagement.Web/Startup.cs
@@ -33,7 +33,6 @@ namespace BotEventManagement.Web
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
