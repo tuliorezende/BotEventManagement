200f44d8b10a265398892a9ef771744e9277f385
tuliorezende <tulio.rezende@outlook.com>
2019-03-21 17:13:57 -0300

    remove unnecessary namespaces

--- Api/BotEventManagement.Models/Database/Address.cs
+++ Api/BotEventManagement.Models/Database/Address.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BotEventManagement.Models.Database
 {
