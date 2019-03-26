ae151dc6b5f152efa9922aabde88ccbcbe5cf757
tuliorezende <tulio.rezende@outlook.com>
2019-03-26 09:28:57 -0300

    put json attributes

--- Api/BotEventManagement.Models/API/UserAuthenticationResponse.cs
+++ Api/BotEventManagement.Models/API/UserAuthenticationResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,10 +7,15 @@ namespace BotEventManagement.Models.API
 {
     public class UserAuthenticationResponse
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+        [JsonProperty("username")]
         public string Username { get; set; }
+        [JsonProperty("nome")]
         public string FirstName { get; set; }
+        [JsonProperty("sobrenome")]
         public string LastName { get; set; }
+        [JsonProperty("token")]
         public string Token { get; set; }
 
     }
